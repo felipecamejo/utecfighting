659e149e4d81298c209d6340e3b0ecf3994dcf40
Lucas Ottonello <164694178+lukovski@users.noreply.github.com>
2025-05-30 19:49:59 -0300

    Velocidad random

--- Union Carlos Lucas/anexo/soto.gd
+++ Union Carlos Lucas/anexo/soto.gd
@@ -5,6 +5,15 @@ var current_state = State.IDLE
 var attack_completed = false
 var retreat_distance = 0.0
 var golpes = 0
+var performing_action := false
+var velocidades = [70, 100, 150, 250]
+signal game_over_triggered
+
+@onready var barraVida = $"../Hud/RivalHud/barraVida"
+@onready var barraStamina = $"../Hud/RivalHud/barraStamina"
+@onready var punch_cooldown_timer: Timer = $punch_cooldown_timer
+@export var vida := 100
+@export var stamina := 100
 
 @export var team: int = 2
 @export var speed = 100
@@ -40,8 +49,9 @@ func _physics_process(_delta):
 			if not attack_completed:
 				perform_attack()
 		State.RETREAT:
-			#$PosicionPrincipal.visible = true
-			move_away_from_player(_delta)
+			move_away_from_player()
+			$AnimatedSprite2D.play("RivalWalk")
+			speed = velocidades.pick_random()
 			if distance >= attack_distance * retreat_distance_multiplier:
 				current_state = State.MOVE
 			
@@ -72,6 +82,46 @@ func move_towards_player(_delta):
 	set_velocity(direction * speed)
 	$AnimationPlayer.play("Caminar")
 	move_and_slide()
+
+var ya_decidio_timer := false  # variable de control para la decisión del timer
+
+func move_away_from_player():
+	var direction = (personaje.global_position - global_position).normalized()
+	set_velocity(-direction * speed)
+	move_and_slide()
+	# Decidir solo una vez en el ciclo RETREAT antes del próximo ataque
+	if not ya_decidio_timer and current_state == State.RETREAT:
+		var random_value = randi_range(1, 3)  # 1, 2 o 3
+		punch_cooldown_timer.one_shot = (random_value <= 2)  # true si 1 o 2 (66%) # SIEMPRE TRUE PARA QUE GOLPEE
+		ya_decidio_timer = true
+		print("Decidió punch_cooldown_timer.one_shot =", punch_cooldown_timer.one_shot)
+
+
+
+func start_attack():
+	performing_action = true
+	$AnimatedSprite2D.play("RivalPunch")
+	$Golpe/HitArea.monitoring = true
+	punch_cooldown_timer.start()
+	await get_tree().create_timer(0.6).timeout
+	current_state = State.RETREAT
+	$Golpe/HitArea.monitoring = false
+	ya_decidio_timer = false  # Reseteamos para la próxima vez que ataque
+	performing_action = false
+
+
+func start_block():
+	performing_action = true
+	$AnimatedSprite2D.play("RivalGuard")
+	await get_tree().create_timer(0.8).timeout
+	current_state = State.MOVE
+	performing_action = false
+
+func start_hit():
+	performing_action = true
+	$AnimatedSprite2D.play("RivalPunched")
+	golpes += 1
+	vida -= 10
 	
 func move_away_from_player(_delta):
 	$AnimationPlayer.play("Caminar")
