01f33acf6b99ed4bf78d0a12104047955da138c4
Lucas Ottonello <164694178+lukovski@users.noreply.github.com>
2025-06-26 11:04:04 -0300

    Menu principal
    Comienzo del menu

---
+++ Union Carlos Lucas/anexo/main_menu.gd
@@ -0,0 +1,32 @@
+extends Control
+
+@export var intro_video: VideoStream
+@export var nivel_1_path: String = "res://scenes/Nivel1.tscn"
+
+@onready var video_player: VideoStreamPlayer = $VideoPlayer
+@onready var jugar_btn: Button = $VBoxContainer/Jugar
+@onready var salir_btn: Button = $VBoxContainer/Salir
+
+func _ready():
+	jugar_btn.pressed.connect(_on_jugar_pressed)
+	salir_btn.pressed.connect(_on_salir_pressed)
+	#video_player.finished.connect(_on_video_finished)
+
+func _on_jugar_pressed():
+	hide_buttons()
+	#play_intro()
+	get_tree().change_scene_to_file(nivel_1_path)
+
+func _on_salir_pressed():
+	get_tree().quit()
+
+func hide_buttons():
+	$VBoxContainer.hide()
+
+func play_intro():
+	video_player.stream = intro_video
+	video_player.visible = true
+	video_player.play()
+
+func _on_video_finished():
+	get_tree().change_scene_to_file(nivel_1_path)
