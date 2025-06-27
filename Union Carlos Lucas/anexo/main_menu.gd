18512ddf8a407fa3258914a50db5fbe4c54f6b55
Lucas Ottonello <164694178+lukovski@users.noreply.github.com>
2025-06-27 20:36:50 -0300

    Cinematica
    Se agrega cinematica y se configura. Comienza menu, va a cinematica y cuando termina empieza el nivel isra

--- Union Carlos Lucas/anexo/main_menu.gd
+++ Union Carlos Lucas/anexo/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @export var intro_video: VideoStream
-@export var nivel_1_path: String = "res://scenes/Nivel1.tscn"
+@export var nivel_1_path: String = "res://NivelIsra.tscn"
 
 @onready var video_player: VideoStreamPlayer = $VideoPlayer
 @onready var jugar_btn: Button = $VBoxContainer/Jugar
@@ -10,12 +10,12 @@ extends Control
 func _ready():
 	jugar_btn.pressed.connect(_on_jugar_pressed)
 	salir_btn.pressed.connect(_on_salir_pressed)
-	#video_player.finished.connect(_on_video_finished)
+	video_player.finished.connect(_on_video_finished)
+
 
 func _on_jugar_pressed():
 	hide_buttons()
-	#play_intro()
-	get_tree().change_scene_to_file(nivel_1_path)
+	play_intro()
 
 func _on_salir_pressed():
 	get_tree().quit()
@@ -24,9 +24,18 @@ func hide_buttons():
 	$VBoxContainer.hide()
 
 func play_intro():
+	$MenuMusic.stop()
+	$ImagenFondo.visible = false
+	$ColorRect.visible = false
+	$ImagenPersonaje.visible = false
 	video_player.stream = intro_video
 	video_player.visible = true
 	video_player.play()
-
+	
 func _on_video_finished():
+	print("Cambiando a:", nivel_1_path)
 	get_tree().change_scene_to_file(nivel_1_path)
+
+	
+	
+	
